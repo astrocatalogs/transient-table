648d19e2dc96dec660348ae586626be4223804a4
James Guillochon <jfg@ucolick.org>
2016-01-11 23:09:44 -0800

    Added more columns, improved responsive behavior, improved column searches.

--- wordpress-transient-table.php
+++ wordpress-transient-table.php
@@ -14,27 +14,81 @@ function sne_catalog() {
 ?>
 	<script>
 	jQuery(document).ready(function() {
-		var colValDict = {};
-		var colInds = [];
-		var advSearchCols = ['z', 'Data'];
+		var floatColValDict = {};
+		var floatColInds = [];
+		var floatSearchCols = ['z', 'Data', 'mmax', 'Mmax', 'v&#9737; (km/s)', 'dL (Mpc)' ];
+		var stringColValDict = {};
+		var stringColInds = [];
+		var stringSearchCols = ['Name', 'Aliases', 'Host Name', 'Instruments/Bands', 'Claimed Type' ];
         jQuery('#example tfoot th').each( function ( index ) {
             if (index == 0) return;
             var title = jQuery(this).text();
-            if (title == "Plots" || title == "vHelio") return;
-			if (advSearchCols.indexOf(title) !== -1)
+			if (floatSearchCols.indexOf(title) !== -1)
 			{
-				colValDict[index] = title;
-				colInds.push(index);
+				floatColValDict[index] = title;
+				floatColInds.push(index);
+			}
+			if (stringSearchCols.indexOf(title) !== -1)
+			{
+				stringColValDict[index] = title;
+				stringColInds.push(index);
 			}
             jQuery(this).html( '<input class="colsearch" type="text" id="'+title+'" placeholder="'+title+'" />' );
         } );
+		jQuery.fn.dataTableExt.oSort['nullable-asc'] = function(a,b) {
+			if (a == '')
+				return 1;
+			else if (b == '')
+				return -1;
+			else
+			{
+				var ia = parseFloat(a);
+				var ib = parseFloat(b);
+				return (ia<ib) ? -1 : ((ia > ib) ? 1 : 0);
+			}
+		}
+		jQuery.fn.dataTableExt.oSort['nullable-desc'] = function(a,b) {
+			if (a == '')
+				return 1;
+			else if (b == '')
+				return -1;
+			else
+			{
+				var ia = parseFloat(a);
+				var ib = parseFloat(b);
+				return (ia>ib) ? -1 : ((ia < ib) ? 1 : 0);
+			}
+		}
 		var table = jQuery('#example').DataTable( {
 			ajax: '../../sne/sne-catalog.json',
-            dom: 'Bfrtlip',
+			columns: [
+				{ "defaultContent": "", "responsivePriority": 5 },
+				{ "data": "name", "type": "string", "responsivePriority": 1 },
+				{ "data": "aliases[, ]", "type": "string" },
+				{ "data": "discoverdate", "type": "date" },
+				{ "data": "maxdate", "type": "date" },
+				{ "data": "maxappmag", "type": "nullable" },
+				{ "data": "maxabsmag", "type": "nullable" },
+				{ "data": "host", "type": "string" },
+				{ "data": "instruments", "type": "string" },
+				{ "data": "redshift", "type": "nullable", "responsivePriority": 4 },
+				{ "data": "hvel", "type": "nullable" },
+				{ "data": "lumdist", "type": "nullable" },
+				{ "data": "claimedtype", "type": "string", "responsivePriority": 3 },
+				{ "data": "data", "type": "html-num", "responsivePriority": 2 },
+				{ "defaultContent": "" },
+			],
+            dom: 'Bfprtlip',
             //colReorder: true,
+			orderMulti: true,
             pagingType: 'simple_numbers',
             pageLength: 50,
-            responsive: true,
+			responsive: {
+				details: {
+					type: 'column',
+					target: -1
+				}
+			},
             select: true,
             lengthMenu: [ [10, 50, 250], [10, 50, 250] ],
             deferRender: true,
@@ -61,18 +115,22 @@ function sne_catalog() {
                 }
             ],
             columnDefs: [ {
+                targets: 0,
                 orderable: false,
-                className: 'select-checkbox',
-                targets: 0
+                className: 'select-checkbox'
             }, {
-                visible: false,
-                targets: [ 'aliases', 'maxdate', 'hvel' ]
-            } ],
+                targets: [ 'aliases', 'maxdate', 'hvel', 'maxabsmag', 'lumdist' ],
+				visible: false,
+			}, {
+				className: 'control',
+				orderable: false,
+				targets: -1
+			} ],
             select: {
                 style:    'os',
                 selector: 'td:first-child'
             },
-            order: [[ 10, "desc" ]]
+            order: [[ 13, "desc" ]]
 		} );
 		function needAdvanced (str) {
 			return (str.indexOf('-') !== -1 || str.indexOf(',') !== -1 || str.indexOf('<') !== -1 || str.indexOf('>') !== -1);
@@ -81,7 +139,8 @@ function sne_catalog() {
             var that = this;
 
             jQuery( 'input', that.footer() ).keyup( function () {
-				if ( colInds.indexOf(index) === -1 ) {
+				if (( floatColInds.indexOf(index) === -1 ) &&
+				    ( stringColInds.indexOf(index) === -1 )) {
 					if ( that.search() !== this.value ) {
 						that.search( this.value );
 					}
@@ -89,6 +148,29 @@ function sne_catalog() {
 				that.draw();
             } );
         } );
+		function advancedStringFilter ( data, id ) {
+			var idObj = document.getElementById(id);
+			if ( idObj === null ) return true;
+			var idString = idObj.value;
+			var splitString = idString.split(',');
+			for ( var i = 0; i < splitString.length; i++ ) {
+				var idStr = splitString[i].trim().toUpperCase();
+				if ( idStr === "" || idStr === NaN ) {
+					if (i === 0) return true;
+				}
+				else {
+					var lowData = String(data).toUpperCase();
+					if ( idStr.indexOf('"') !== -1 ) {
+						idStr = idStr.replace(/"/g, '');
+						if ( lowData === idStr || (idStr === "" && i === 0) ) return true;
+					}
+					else {
+						if ( lowData.indexOf(idStr) !== -1 ) return true;
+					}
+				}
+			}
+			return false;
+		}
 		function advancedFloatFilter ( data, id ) {
 			var idObj = document.getElementById(id);
 			if ( idObj === null ) return true;
@@ -102,35 +184,56 @@ function sne_catalog() {
 					var maxStr = splitRange[1].trim();
 					var minVal = minStr * 1.0;
 					var maxVal = maxStr * 1.0;
-					if ( (minStr !== '' && data < minVal) || (maxStr !== '' && data > maxVal) ) return false;
+					if (minStr !== '') {
+						if (!( (minStr !== '' && data < minVal) || (maxStr !== '' && data > maxVal) )) return true;
+					}
 				}
-				else
-				{
-					var idStr = splitString[i].replace(/[<>]/g, '').trim();
-					if ( idStr === "" || idStr === NaN ) continue;
+				var idStr = splitString[i].replace(/[<=>]/g, '').trim();
+				if ( idStr === "" || idStr === NaN ) {
+					if (i === 0) return true;
+				}
+				else {
 					idVal = idStr * 1.0;
-					if ( splitString[i].indexOf('<') !== -1 )
+					if ( splitString[i].indexOf('<=') !== -1 )
 					{
-						if ( idVal <= data ) return false;
+						if ( idVal >= data ) return true;
+					}
+					else if ( splitString[i].indexOf('<') !== -1 )
+					{
+						if ( idVal > data ) return true;
+					}
+					else if ( splitString[i].indexOf('>=') !== -1 )
+					{
+						if ( idVal <= data ) return true;
 					}
 					else if ( splitString[i].indexOf('>') !== -1 )
 					{
-						if ( idVal >= data ) return false;
+						if ( idVal < data ) return true;
 					}
 					else
 					{
-						if ( data.indexOf(idStr) === -1 ) return false;
+						if ( idStr.indexOf('"') !== -1 ) {
+							idStr = String(idStr.replace(/"/g, '').trim());
+							if ( data === idStr || (idStr === "" && i === 0) ) return true;
+						}
+						else {
+							if ( data.indexOf(idStr) !== -1 ) return true;
+						}
 					}
 				}
 			}
-			return true;
+			return false;
 		}
 		jQuery.fn.dataTable.ext.search.push(
 			function( oSettings, aData, iDataIndex ) {
 				for ( var i = 0; i < aData.length; i++ )
 				{
-					if ( colInds.indexOf(i) === -1 ) continue;
-					if ( !advancedFloatFilter( aData[i], colValDict[i] ) ) return false;
+					if ( floatColInds.indexOf(i) !== -1 ) {
+						if ( !advancedFloatFilter( aData[i], floatColValDict[i] ) ) return false;
+					}
+					if ( stringColInds.indexOf(i) !== -1 ) {
+						if ( !advancedStringFilter( aData[i], stringColValDict[i] ) ) return false;
+					}
 				}
 				return true;
 			}
